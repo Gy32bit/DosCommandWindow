a50d1f6467265ad9573d9bca297907946a32b2c6
Gy32bit <107751361+Gy32bit@users.noreply.github.com>
2022-12-17 20:11:13 +0800

    Add files via upload

--- DosCommandWindow.cpp
+++ DosCommandWindow.cpp
@@ -3,9 +3,54 @@
 
 using namespace std;
 
+const int password = 1470369258;
+int in_password;
+string name = "Gy32bit";
+string in_name;
+string command;
+
+
+int start(bool is_admin) {
+    cout << "DosCommandWindow" << endl;
+    cout << "copyright(c) Gy32bit  ||  1.0" << endl;
+    cout << "锅鳖!" << endl;
+
+    return 0;
+}
+
+int input_command() {
+    cout << ">>> ";
+    cin >> command;
+
+    return 0;
+}
+
+//down to see commands:
+
+int pause() {
+    system("pause");
+    
+
+    return 0;
+}
+
 int main()
 {
-    
+    start(0);
+    while (command != "close") {
+        input_command();
+        if (command == "close") {
+            continue;
+
+        }
+        if (command == "pause") {
+            pause();
+
+            continue;
+
+        }
+    }
+    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>|:-)|<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
 
     return 0;
 }
